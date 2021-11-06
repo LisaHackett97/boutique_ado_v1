1a2dbb5ad5037d10cf2168793f946cdfc6805617
LisaHackett97 <lisa-hackett@hotmail.com>
2021-11-06 09:13:56 +0000

    Add checkout views and templates

--- checkout/models.py
+++ checkout/models.py
@@ -1,11 +1,11 @@
 import uuid
+
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
 from products.models import Product
 
-# Create your models here.
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
@@ -25,16 +25,16 @@ class Order(models.Model):
 
     def _generate_order_number(self):
         """
-        Generate a randon, unique order number using UUID
+        Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
         """
         Update grand total each time a line item is added,
-        accounting for delivery costs 
+        accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggreagate(Sum('lineitem_total'))['lineitem_total_sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -45,7 +45,7 @@ class Order(models.Model):
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the order number
-        if it hasn't been set already
+        if it hasn't been set already.
         """
         if not self.order_number:
             self.order_number = self._generate_order_number()
@@ -64,8 +64,8 @@ class OrderLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the order number
-        if it hasn't been set already
+        Override the original save method to set the lineitem total
+        and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
